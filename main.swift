78705707d8515e70d63d2d559dd8eb0982f01e23
vtra91 <melnikov0109@gmail.com>
2025-05-01 12:18:14 +0800

    Update main.swift

--- main.swift
+++ main.swift
@@ -2,7 +2,25 @@ import Foundation
 
 typealias Matrix = [[Int]]
 
-// Функция для поворота подматрицы 2x2 по часовой стрелке (Rot)
+var rotCount = 0
+var mRotCount = 0
+var swapHCount = 0
+var swapVCount = 0
+var operationLog: [String] = []
+
+func generateRandomMatrix(_ rows: Int, _ columns: Int, minValue: Int = 0, maxValue: Int = 99) -> [[Int]] {
+    var matrix = [[Int]]()
+    for _ in 0..<rows {
+        var row = [Int]()
+        for _ in 0..<columns {
+            let randomNumber = Int.random(in: minValue...maxValue)
+            row.append(randomNumber)
+        }
+        matrix.append(row)
+    }
+    return matrix
+}
+
 func Rot(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     
@@ -15,19 +33,17 @@ func Rot(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     matrix[row][col+1] = a
     matrix[row+1][col+1] = b
     matrix[row+1][col] = c
+    
+    rotCount += 1
+    operationLog.append("Rot(\(row), \(col))")
 }
 
 func flattenAndSort(_ matrix: Matrix) -> [Int] {
-    // Преобразуем двумерный массив в одномерный
     let flattenedArray = matrix.flatMap { $0 }
-    
-    // Сортируем массив
     let sortedArray = flattenedArray.sorted()
-    
     return sortedArray
 }
 
-// Функция для поворота подматрицы 2x2 против часовой стрелки (3*Rot)
 func mRot(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     
@@ -40,9 +56,11 @@ func mRot(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     matrix[row][col+1] = c
     matrix[row+1][col+1] = d
     matrix[row+1][col] = a
+    
+    mRotCount += 1
+    operationLog.append("mRot(\(row), \(col))")
 }
 
-// Функция для горизонтального обмена элементов подматрицы 2x2 (swapH)
 func swapH(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     
@@ -55,9 +73,11 @@ func swapH(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     matrix[row][col+1] = c
     matrix[row+1][col] = a
     matrix[row+1][col+1] = b
+    
+    swapHCount += 1
+    operationLog.append("swapH(\(row), \(col))")
 }
 
-// Функция для вертикального обмена элементов подматрицы 2x2 (swapV)
 func swapV(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     
@@ -70,16 +90,17 @@ func swapV(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     matrix[row][col+1] = a
     matrix[row+1][col+1] = d
     matrix[row+1][col] = c
+    
+    swapVCount += 1
+    operationLog.append("swapV(\(row), \(col))")
 }
 
-// Функция для диагонального преобразования (diagL)
 func diagL(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     mRot(&matrix, aroundA: row, col)
     swapH(&matrix, aroundA: row, col)
 }
 
-// Функция для диагонального преобразования (diagB)
 func diagB(_ matrix: inout Matrix, aroundA row: Int, _ col: Int) {
     guard matrix.count > row + 1 && matrix[0].count > col + 1 else { return }
     mRot(&matrix, aroundA: row, col)
@@ -95,11 +116,6 @@ func printMatrix(_ matrix: Matrix) {
 }
 
 class Frame {
-    /*
-     # Z Z
-     # # #
-     # # #
-     */
     func swap_00_01(_ matrix: inout Matrix) {
         let row = 0
         let col = 1
@@ -110,6 +126,7 @@ class Frame {
         mRot(&matrix, aroundA: row, col)
         diagB(&matrix, aroundA: row, col)
     }
+    
     func swap_01_02(_ matrix: inout Matrix) {
         let row = 0
         let col = 0
@@ -120,6 +137,7 @@ class Frame {
         Rot(&matrix, aroundA: row, col)
         diagL(&matrix, aroundA: row, col)
     }
+    
     func swap_00_10(_ matrix: inout Matrix) {
         let row = 1
         let col = 0
@@ -130,6 +148,7 @@ class Frame {
         Rot(&matrix, aroundA: row, col)
         diagB(&matrix, aroundA: row, col)
     }
+    
     func swap_10_20(_ matrix: inout Matrix) {
         let row = 0
         let col = 0
@@ -140,6 +159,7 @@ class Frame {
         mRot(&matrix, aroundA: row, col)
         diagL(&matrix, aroundA: row, col)
     }
+    
     func swap_02_12(_ matrix: inout Matrix) {
         let row = 1
         let col = 1
@@ -150,6 +170,7 @@ class Frame {
         mRot(&matrix, aroundA: row, col)
         diagL(&matrix, aroundA: row, col)
     }
+    
     func swap_12_22(_ matrix: inout Matrix) {
         let row = 0
         let col = 1
@@ -160,6 +181,7 @@ class Frame {
         Rot(&matrix, aroundA: row, col)
         diagB(&matrix, aroundA: row, col)
     }
+    
     func swap_20_21(_ matrix: inout Matrix) {
         let row = 1
         let col = 1
@@ -170,6 +192,7 @@ class Frame {
         Rot(&matrix, aroundA: row, col)
         diagL(&matrix, aroundA: row, col)
     }
+    
     func swap_21_22(_ matrix: inout Matrix) {
         let row = 1
         let col = 0
@@ -183,7 +206,7 @@ class Frame {
 }
 
 func casilkaLOWEST(_ matrix: inout Matrix, _ i: Int, _ j: Int) {
-    var temp = Frame()
+    let temp = Frame()
     switch (i, j) {
     case (0, 0):
         temp.swap_00_10(&matrix)
@@ -214,41 +237,95 @@ func casilkaLOWEST(_ matrix: inout Matrix, _ i: Int, _ j: Int) {
 }
 
 func casilkaJOIN(_ matrix: inout Matrix, _ i: Int, _ j: Int, _ str: [Int], _ flag: Int) {
-    var temp = Frame()
+    let temp = Frame()
     if flag == 1 {
         switch (i, j) {
         case (0, 0):
-            temp.swap_00_10(&matrix)
+            if(matrix[1][0] == str[1] || matrix[1][0] == str[2]) {
+                temp.swap_00_01(&matrix)
+                temp.swap_01_02(&matrix)
+                temp.swap_02_12(&matrix)
+                temp.swap_12_22(&matrix)
+                temp.swap_21_22(&matrix)
+            }
+            else { temp.swap_00_10(&matrix) }
         case (0, 1):
-            temp.swap_00_01(&matrix)
-            temp.swap_00_10(&matrix)
+            if(matrix[1][0] == str[1] || matrix[1][0] == str[2] ||
+               matrix[0][0] == str[1] || matrix[0][0] == str[2] ) {
+                temp.swap_01_02(&matrix)
+                temp.swap_02_12(&matrix)
+                temp.swap_12_22(&matrix)
+                temp.swap_21_22(&matrix)
+            }
+            else {temp.swap_00_01(&matrix)
+                temp.swap_00_10(&matrix) }
         case (0, 2):
-            temp.swap_01_02(&matrix)
-            temp.swap_00_01(&matrix)
-            temp.swap_00_10(&matrix)
+            if(matrix[1][0] == str[1] || matrix[1][0] == str[2] ||
+               matrix[0][0] == str[1] || matrix[0][0] == str[2] ||
+               matrix[0][1] == str[1] || matrix[0][1] == str[2]) {
+                temp.swap_02_12(&matrix)
+                temp.swap_12_22(&matrix)
+                temp.swap_21_22(&matrix)
+            } else {
+                temp.swap_01_02(&matrix)
+                temp.swap_00_01(&matrix)
+                temp.swap_00_10(&matrix)
+            }
         case (1, 2):
-            temp.swap_02_12(&matrix)
-            temp.swap_01_02(&matrix)
-            temp.swap_00_01(&matrix)
-            temp.swap_00_10(&matrix)
+            if(matrix[2][1] == str[1] || matrix[2][1] == str[2] ||
+               matrix[2][2] == str[1] || matrix[2][2] == str[2] ) {
+                temp.swap_02_12(&matrix)
+                temp.swap_01_02(&matrix)
+                temp.swap_00_01(&matrix)
+                temp.swap_00_10(&matrix)
+            }
+            else {
+                temp.swap_12_22(&matrix)
+                temp.swap_21_22(&matrix)
+            }
         case (2, 2):
-            temp.swap_21_22(&matrix)
+            if(matrix[2][1] == str[1] || matrix[2][1] == str[2]) {
+                temp.swap_12_22(&matrix)
+                temp.swap_02_12(&matrix)
+                temp.swap_01_02(&matrix)
+                temp.swap_00_01(&matrix)
+                temp.swap_00_10(&matrix)
+            }
+            else {
+                temp.swap_21_22(&matrix) }
         default:
             return
         } }
         else if flag == 2 {
             switch (i, j) {
-            case (1, 0):
-                temp.swap_00_10(&matrix)
             case (0, 1):
-                temp.swap_00_01(&matrix)
+                if matrix[0][0] == str[3] ||  matrix[0][0] == str[4] ||  matrix[0][0] == str[5] {
+                    temp.swap_01_02(&matrix)
+                    temp.swap_02_12(&matrix)
+                    temp.swap_12_22(&matrix)
+                }
+                else {temp.swap_00_01(&matrix) }
             case (0, 2):
-                temp.swap_01_02(&matrix)
-                temp.swap_00_01(&matrix)
+                if matrix[0][0] == str[3] ||  matrix[0][0] == str[4] ||  matrix[0][0] == str[5] ||
+                    matrix[0][1] == str[3] ||  matrix[0][1] == str[4] ||  matrix[0][1] == str[5]
+                {
+                    temp.swap_02_12(&matrix)
+                    temp.swap_12_22(&matrix)
+                }
+                else {
+                    temp.swap_01_02(&matrix)
+                    temp.swap_00_01(&matrix)
+                }
             case (1, 2):
-                temp.swap_12_22(&matrix)
-            case (2, 1):
-                temp.swap_21_22(&matrix)
+                if matrix[2][2] == str[3] ||  matrix[2][2] == str[4] ||  matrix[2][2] == str[5]{
+                    temp.swap_02_12(&matrix)
+                    temp.swap_01_02(&matrix)
+                    temp.swap_00_01(&matrix)
+                }
+                else {
+                    temp.swap_12_22(&matrix)
+                }
+
             default:
                 return
             }
@@ -256,15 +333,10 @@ func casilkaJOIN(_ matrix: inout Matrix, _ i: Int, _ j: Int, _ str: [Int], _ fla
         }
         else if flag == 3{
             switch (i, j) {
-            case (1, 0):
-                temp.swap_00_10(&matrix)
-                temp.swap_00_01(&matrix)
+
             case (0, 0):
                 temp.swap_00_01(&matrix)
             case (2, 2):
-                temp.swap_12_22(&matrix)
-            case (2, 1):
-                temp.swap_21_22(&matrix)
                 temp.swap_12_22(&matrix)
 
             default:
@@ -296,42 +368,34 @@ func casilkaJOIN(_ matrix: inout Matrix, _ i: Int, _ j: Int, _ str: [Int], _ fla
             return
         }
     }
-    }
-
+}
 
 func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
     var flag: Int = 1
-    // Проверяем, что матрица 3x3
+    
     guard matrix.count == 3, matrix.allSatisfy({ $0.count == 3 }) else {
         return
     }
     
-    var minMX = sortedMX.min() ?? 0
-    
-
+    let minMX = sortedMX.min() ?? 0
     
     for i in 0..<3 {
         for j in 0..<3 {
-            // Пропускаем центральную ячейку (1,1)
             if i == 1 && j == 1 {
                 continue
             }
-            if matrix[1][1] == minMX && (matrix[0][0] == sortedMX[3] || matrix[0][0] == sortedMX[4] || matrix[0][0] == sortedMX[5]) {
-                diagB(&matrix, aroundA: 0, 0)
+            if matrix[1][1] != sortedMX[3] && matrix[1][1] != sortedMX[4] && matrix[1][1] != sortedMX[5]{
+                while matrix[1][1] != sortedMX[3] && matrix[1][1] != sortedMX[4] && matrix[1][1] != sortedMX[5] {
+                    Rot(&matrix, aroundA: 0, 0)
+                    mRot(&matrix, aroundA: 0, 1)
+                    Rot(&matrix, aroundA: 1, 0)
+                    mRot(&matrix, aroundA: 1, 1)
+                }
             }
-            if matrix[1][1] == minMX && (matrix[0][2] == sortedMX[3] || matrix[0][2] == sortedMX[4] || matrix[0][2] == sortedMX[5]) {
-                diagL(&matrix, aroundA: 0, 1)
-            }
-            if matrix[1][1] == minMX && (matrix[2][2] == sortedMX[3] || matrix[2][2] == sortedMX[4] || matrix[2][2] == sortedMX[5]) {
-                diagB(&matrix, aroundA: 1, 1)
-            }
-            if matrix[1][1] == minMX && (matrix[2][0] == sortedMX[3] || matrix[2][0] == sortedMX[4] || matrix[2][0] == sortedMX[5]) {
-                diagL(&matrix, aroundA: 1, 0)
-            }
+            
             let currentValue = matrix[i][j]
             if currentValue == minMX {
                 casilkaLOWEST(&matrix, i, j)
-
             }
         }
     }
@@ -341,7 +405,6 @@ func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
         case 1:
             for i in 0..<3 {
                 for j in 0..<3 {
-                    // Пропускаем центральную ячейку (1,1)
                     if i == 1 && j == 1 {
                         continue
                     }
@@ -354,7 +417,6 @@ func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
         case 2:
             for i in 0..<3 {
                 for j in 0..<3 {
-                    // Пропускаем центральную ячейку (1,1)
                     if i == 1 && j == 1 {
                         continue
                     }
@@ -367,7 +429,6 @@ func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
         case 3:
             for i in 0..<3 {
                 for j in 0..<3 {
-                    // Пропускаем центральную ячейку (1,1)
                     if i == 1 && j == 1 {
                         continue
                     }
@@ -380,7 +441,6 @@ func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
         case 4:
             for i in 0..<3 {
                 for j in 0..<3 {
-                    // Пропускаем центральную ячейку (1,1)
                     if i == 1 && j == 1 {
                         continue
                     }
@@ -394,35 +454,83 @@ func findMinExcludingCenter(_ matrix: inout Matrix, _ sortedMX: [Int])  {
         }
         flag+=1
     }
-    
 }
 
-// Пример использования
-//var original: Matrix = [
-//    [7, 26, 3],
-//    [15, 9, 10],
-//    [22, 16, 4]
-//]
+func sortLargeMatrixBySubmatrices(_ matrix: [[Int]], _ mxCol: Int, _ mxRow: Int) -> [[Int]] {
+    guard matrix.count >= 3, matrix[0].count >= 3 else {
+        fatalError("Матрица должна быть хотя бы 3×3")
+    }
+    var resultMatrix = matrix
+    let limit = (mxCol-3+1)
+    
+    for _ in 0...limit {
+        for i in 0...(matrix.count - 3) {
+            for j in 0...(matrix[i].count - 3) {
+                var submatrix = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+                
+                for k in 0..<3 {
+                    for l in 0..<3 {
+                        submatrix[k][l] = resultMatrix[i + k][j + l]
+                    }
+                }
+                
+                let sortedValues = flattenAndSort(submatrix)
+                findMinExcludingCenter(&submatrix, sortedValues)
 
+                for k in 0..<3 {
+                    for l in 0..<3 {
+                        resultMatrix[i + k][j + l] = submatrix[k][l]
+                    }
+                }
+            }
+        }
+    }
+    
+    return resultMatrix
+}
+
+// Reset counters before each run
+func resetCounters() {
+    rotCount = 0
+    mRotCount = 0
+    swapHCount = 0
+    swapVCount = 0
+    operationLog = []
+}
+
+// Test with the example matrix
+resetCounters()
 var original: Matrix = [
-    [34, 234, 7],
-    [4, 1, 2],
-    [99, 9, 86]
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
 ]
 
-
-print("Original matrix:")
+print("Оригинальная матрица 3х3:")
 printMatrix(original)
-
-
-
-
 var sortedMX = flattenAndSort(original)
-print(sortedMX)
-
-var minMX = sortedMX.min() ?? 0
-print(minMX)
 
 findMinExcludingCenter(&original, sortedMX)
+print("Отсортированная матрица 3х3:")
 printMatrix(original)
 
+var mxSize = (col: 50, row: 50)
+
+let randMX = generateRandomMatrix(mxSize.col, mxSize.row)
+print("\nОригинальная рандомная матрица \(mxSize.col)х\(mxSize.row):")
+printMatrix(randMX)
+
+let tempRes = sortLargeMatrixBySubmatrices(randMX,mxSize.col,mxSize.row)
+print("\nОтсортированная рандомная матрица \(mxSize.col)х\(mxSize.row):")
+printMatrix(tempRes)
+
+print("\nКоличество операций:")
+print("Rot: \(rotCount)")
+print("mRot: \(mRotCount)")
+print("swapH: \(swapHCount)")
+print("swapV: \(swapVCount)")
+print("Всего: \(rotCount + mRotCount + swapHCount + swapVCount)")
+
+// TIO.RUN не позволяет вывести весь набор команд (объём вывода превышает 128 кб)
+//print("\nПроизведенные операции:")
+//print(operationLog.joined(separator: " "))
